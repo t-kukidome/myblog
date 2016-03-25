0671196faeca2de63b66137b2e434b5b8a98e00c
久木留　貴士 <t-kukidome@towatech.net>
2016-03-26 03:26:53 +0900

    registeration function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  get 'home/index'
+  root 'home#index'
   #resources :users do
   #  resources :articles
   #end
@@ -10,7 +13,7 @@ Rails.application.routes.draw do
 
 
 
-  root 'articles#index'
+  #root 'articles#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
