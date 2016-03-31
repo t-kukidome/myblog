24936afabb00f3309334639e0c9f6e237f5a7efe
久木留　貴士 <t-kukidome@towatech.net>
2016-03-31 09:45:23 +0900

    username added in devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   #before_action :authenticate_user!
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     articles_path
   end
- # def after_sign_out_path_for resource
- #   root_path
- # end
+  def after_sign_out_path_for resource
+    new_user_session_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:username, :password)}
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:username, :email, :password, :password_confirmation)}
+  end
 
 end
