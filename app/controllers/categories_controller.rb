5d82072fb4b5ee4d511e62fff8d77804789c6f42
Takashi Kukidome <t-kukidome@towatech.net>
2016-04-14 12:39:54 +0900

    categories controller routes changed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,10 +3,6 @@ class CategoriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    redirect_to articles_path
-  end
-
-  def new
     @categories = Category.all.includes(:articles).order(:id)
   end
 
